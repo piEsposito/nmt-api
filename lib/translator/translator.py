0097494acbad411503a7315102532ecddcf40c3c
Pi Esposito <piero.skywalker@gmail.com>
2020-11-24 10:53:22 -0300

    add translator class

---
+++ lib/translator/translator.py
@@ -0,0 +1,22 @@
+class Translator:
+    def __init__(self,
+                 mname="Helsinki-NLP/opus-mt-en-roa"):
+        
+        self.model = AutoModelForSeq2SeqLM.from_pretrained(mname)
+        self.tokenizer = AutoTokenizer.from_pretrained(mname)
+    
+    def translate(self,
+                  text):
+        
+        tac = time.time()
+        
+        as_tensor = self.tokenizer.prepare_seq2seq_batch(src_texts=[text])
+        gen = self.model.generate(**as_tensor)
+        translated = self.tokenizer.batch_decode(gen, skip_special_tokens=True)[0]
+        
+        tic = time.time()
+        
+        return {
+            'translation': translated,
+            'elapsed_time': tic - tac
+        }
